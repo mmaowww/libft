26ea64caa7c8d2f5f2e548185cd93e370d17b6ef
Emilie Betrix <ebetrix@c1r13s7.42lausanne.ch>
2021-10-24 18:06:37 +0200

    Sunday

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,20 +6,51 @@
 /*   By: ebetrix <marvin@42lausanne.ch>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/16 17:49:16 by ebetrix           #+#    #+#             */
-/*   Updated: 2021/10/22 16:56:40 by ebetrix          ###   ########.fr       */
+/*   Updated: 2021/10/24 18:05:14 by ebetrix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //#include 'libft.h'
 #include <stdio.h>
+#include <unistd.h> // pour write
 
-size_t	ft_strlcpy(char *restrict dest, const char *restric src, size_t dstsize)
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
-	while (src[i] != '\0' || dst[i] != '\0')
+	size_t	i;
+
+	i = 0;
+	write(1, "toto", 4);
+	if (dstsize == 0)
 	{
+		while (src[i] != '\0')
+			i++;
+		return (i);
+	}
+		printf("i = %zu", i);
+	while (i < dstsize - 1 && src[i] != '\0')
+	{
+		printf("i = %zu", i);
+		write (1, "tata", 4);
 		dst[i] = src[i];
 		i++;
 	}
+	write(1, "titi", 4);
 	dst[i] = '\0';
-	retun (i);
+	return (i);
+}
+
+int	main()
+{
+	char	*test;
+	const char	*test2;
+	size_t	size;
+
+	test = "123456789";
+	test2 = "abcde";
+	size = 5;
+
+	printf("1ere: %s\n", test);
+	printf("2eme: %s\n", test2);
+	ft_strlcpy(test, test2, size);
+	printf("New: %s\n", test);
 }
--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -6,19 +6,19 @@
 /*   By: ebetrix <marvin@42lausanne.ch>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/16 17:50:26 by ebetrix           #+#    #+#             */
-/*   Updated: 2021/10/22 15:21:38 by ebetrix          ###   ########.fr       */
+/*   Updated: 2021/10/24 17:04:48 by ebetrix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include 'libft.h'
-#include <stdio.h>
+#include 'libft.h'
+//#include <stdio.h>
 
 char	*ft_strrchr(const char *s, int c)
 {
 	int	i;
 
 	i = 0;
-	while (*s != '\0')
+	while (*s)
 	{
 		i++;
 		s++;
@@ -26,20 +26,20 @@ char	*ft_strrchr(const char *s, int c)
 	while (i > 0)
 	{
 		if (*s == (char)c)
-			return ((char*)s);
+			return ((char *)s);
 		i--;
 		s--;
 	}
 	return (0);
 }
 
-int	main()
+/*int	main()
 {
 	const char	*s;
 	int	c;
 
 	s = "salut ca va";
-	c = 97;
+	c = 117;
 	printf("%s\n", ft_strrchr(s, c));
 	return (0);
-}
+}*/
