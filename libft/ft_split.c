6dd9dba62ff1cf0b56387f1d9607ca2376260888
Emilie Betrix <ebetrix@c1r13s4.42lausanne.ch>
2021-11-14 19:41:35 +0100

    Emilie a bien travaille

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,17 +6,16 @@
 /*   By: ebetrix <marvin@42lausanne.ch>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/30 15:51:22 by ebetrix           #+#    #+#             */
-/*   Updated: 2021/11/14 18:48:26 by ebetrix          ###   ########.fr       */
+/*   Updated: 2021/11/14 19:40:28 by ebetrix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-
 int	ft_count_nbr_words(char const *s, char c)
 {
 	int	i;
-	int j;
+	int	j;
 	int	k;
 
 	i = 0;
@@ -45,31 +44,53 @@ size_t	ft_count_len_word(char const *s, char c)
 	return (i);
 }
 
+void	*ft_free(char **s1, int i)
+{
+	while (i--)
+		free(s1[i]);
+	free(s1);
+	return (0);
+}
+
 char	**ft_split(char const *s, char c)
 {
 	char	**s1;
-	int	i;
+	int		i;
+	int		j;
 
+	if (!s)
+		return (0);
 	i = 0;
-	s1 = malloc(sizeof(char*)*(ft_count_nbr_words(s, c) + 1));
+	j = ft_count_nbr_words(s, c);
+	s1 = malloc(sizeof(char *) * (ft_count_nbr_words(s, c) + 1));
 	if (!s1)
 		return (0);
-	while (i < ft_count_nbr_words(s, c))
+	while (i < j)
 	{
-		*s1[i] = ft_substr(s, 0, ft_count_len_word(s, c);
+		while (*s == c && *s)
+			s++;
+		s1[i] = ft_substr(s, 0, ft_count_len_word(s, c));
+		if (s1[i] == NULL)
+			return (ft_free(s1, i));
 		s = s + ft_count_len_word(s, c);
 		i++;
 	}
-	//free
+	s1[i] = "NULL";
 	return (s1);
 }
 
-
-int	main(void)
+/*int	main(void)
 {
-	printf("%d\n", ft_countwords("Je vais a la plage", ' '));
-	printf("%d\n", ft_countwords("      Je vais a la plage.", ' '));
-	printf("%d\n", ft_countwords("Je vais a la       plage.", ' '));
-	printf("%d\n", ft_countwords("Je vais a la plage.   ", ' '));
+	int		i;
+	char	**c;
+
+	printf("%d\n", ft_count_nbr_words("Je vais a la plage.", ' '));
+	c = ft_split("   Je vais    a la plage.   ", ' ');
+	i = 0;
+	while (i < 5)
+	{
+		printf("%s\n", c[i]);
+		i++;
+	}
 	return (0);
-}
+}*/
